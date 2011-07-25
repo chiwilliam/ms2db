cd3e00a9b43f2446ab57d07292e9dd2e6e53b6b3
whemurad@gmail.com <whemurad@gmail.com@8c4e79b5-e7d1-68c0-2b19-dc6ff0ecc16c>
2011-07-25 15:34:56 +0000

    MS2DB+ - footer changes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <p>MS2DB+ was partially supported by funding from the National Science Foundation (NSF) under grant IIS-0644418 (CAREER).</p>
-    <p><a href="copyright.php">Copyright © 2010</a> William Murad and Rahul Singh. All Rights Reserved.</p>
+    <p><a href="copyright.php">Copyright © 2010</a> <a href="http://williammurad.com">William Murad</a> and Rahul Singh. All Rights Reserved.</p>
 </div>
 <div id="footerbottom">
     <img src="images/footerbottom.png" />
