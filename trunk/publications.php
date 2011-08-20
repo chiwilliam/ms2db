11fc7116d3299d0e6fea4147217a755c502c29db
whemurad@gmail.com <whemurad@gmail.com@8c4e79b5-e7d1-68c0-2b19-dc6ff0ecc16c>
2011-08-20 18:21:37 +0000

    MS2DB+ - FAQ and updates on Help

--- trunk/publications.php
+++ trunk/publications.php
@@ -27,6 +27,7 @@
                     <li class="selected"><a href="publications.php" onmouseover="Tip('MS2DB+ Publications')" onmouseout="UnTip()">Publications</a></li>
                     <li><a href="contactus.php" onmouseover="Tip('MS2DB+ Contact Us')" onmouseout="UnTip()">Contact Us</a></li>
                     <li><a href="help.php" onmouseover="Tip('MS2DB+ Help')" onmouseout="UnTip()">Help</a></li>
+                    <li><a href="faq.php" onmouseover="Tip('MS2DB+ FAQ')" onmouseout="UnTip()">FAQ</a></li>
                 </ul>
             </div>
             <!-- End Navigation -->
